bbbee85cb42b4af9b0bf17b6daa99ce9fbff0b43
Khamitov02 <78606737+Khamitov02@users.noreply.github.com>
2021-05-06 12:03:53 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/igra.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/igra.dir/src/class.cpp.obj"
+  "CMakeFiles/igra.dir/src/main.cpp.obj"
+  "CMakeFiles/igra.dir/src/menu.cpp.obj"
+  "igra.exe"
+  "igra.exe.manifest"
+  "igra.pdb"
+  "libigra.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/igra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
